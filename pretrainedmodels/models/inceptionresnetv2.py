5bcb0041082674f47d4d2781025ec45820a76976
Dominic Silk <59667213+silkdom@users.noreply.github.com>
2020-06-09 20:51:29 -0600

    Update inceptionresnetv2.py

--- pretrainedmodels/models/inceptionresnetv2.py
+++ pretrainedmodels/models/inceptionresnetv2.py
@@ -10,7 +10,7 @@ __all__ = ['InceptionResNetV2', 'inceptionresnetv2']
 pretrained_settings = {
     'inceptionresnetv2': {
         'imagenet': {
-            'url': 'http://data.lip6.fr/cadene/pretrainedmodels/inceptionresnetv2-520b38e4.pth',
+            'url': 'https://drive.google.com/file/d/1fLAB-h5uLi93UUFRV3Os5cZAFwlLNUIg/view?usp=sharing',
             'input_space': 'RGB',
             'input_size': [3, 299, 299],
             'input_range': [0, 1],
@@ -19,7 +19,7 @@ pretrained_settings = {
             'num_classes': 1000
         },
         'imagenet+background': {
-            'url': 'http://data.lip6.fr/cadene/pretrainedmodels/inceptionresnetv2-520b38e4.pth',
+            'url': 'https://drive.google.com/file/d/1fLAB-h5uLi93UUFRV3Os5cZAFwlLNUIg/view?usp=sharing',
             'input_space': 'RGB',
             'input_size': [3, 299, 299],
             'input_range': [0, 1],
